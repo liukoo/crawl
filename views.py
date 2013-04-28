df27fef5d0907e1fed3b442eba9b249c29f6ecac
liukoo <lqf800@qq.com>
2013-04-28 15:31:38 +0800

    init

--- views.py
+++ views.py
@@ -1,8 +1,10 @@
+#coding:utf-8
 from django.http import HttpResponse
 from  django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 import httplib2,MySQLdb,re
 from urllib import urlencode
+import json
 from admin.models import *
 
 def index(request):
@@ -12,12 +14,17 @@ def index(request):
 def add_task(request):
     url = request.POST["store_url"]
     mail = request.POST["mail_address"]
-    rule = re.compile(r"(?:http://)?([^/]*)")
+    rule = re.compile(r"(?:http://)?([^/]+)")
+    rule2 = re.compile(r"\w+(?:\.\w+)?@\w+\.\w+")
     url = rule.match(url)
     if url:
         url = url.group(1)
     else:
-        return HttpResponse("{'stat':'error'}")
+        msg = {'stat':'error','msg':'店铺地址错误'}
+        return HttpResponse(json.dumps(msg,ensure_ascii=False))
+    if not rule2.match(mail):
+        msg = {'stat':'error','msg':'邮箱格式错误'}
+        return HttpResponse(json.dumps(msg,ensure_ascii=False))
     http = httplib2.Http()
     data={'project':"dirbot","spider":"taobao"}
     header={'Content-Type': 'application/x-www-form-urlencoded'}
