ec6dd9ebe4511e4a7a406774aa98ab6e2584d6e7
liukoo <lqf800@qq.com>
2013-05-15 22:25:39 +0800

    test

--- urls.py
+++ urls.py
@@ -10,7 +10,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'crawl.views.home', name='home'),
     # url(r'^crawl/', include('crawl.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
